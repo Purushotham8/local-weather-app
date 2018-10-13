b0a0d81aff38cb30948cd8aa8b4295d20221d783
Purushotham <purushotham.815@gmail.com>
2018-10-13 18:45:44 +0530

    commit 2 weather app

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -10,11 +10,19 @@ import { WeatherService } from 'src/app/weather/weather.service';
 export class CurrentWeatherComponent implements OnInit {
   current: ICurrentWeather;
   constructor(private weatherService: WeatherService) {
+    this.current = {
+      city: '',
+      country: '',
+      date: 0,
+      image: '',
+      temperature: 0,
+      description: ''
+    };
   }
 
   ngOnInit() {
-    this.weatherService.getCurrentWeather('Bethesda', 'US').subscribe(
-      (data) => this.current = data
-    );
+    this.weatherService
+      .getCurrentWeather('Bethesda', 'US')
+      .subscribe(data => (this.current = data));
   }
 }
